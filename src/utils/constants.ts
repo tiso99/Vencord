3647d15b0f5da612bbb5191200249f9de862a65c
Neptune <cloudflame01@gmail.com>
2025-04-04 22:50:58 -0400

    Added tiso99(1251221319085854808)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -589,6 +589,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "samsam",
         id: 836452332387565589n,
     },
+    tiso99: {
+        name: "tiso99",
+        id: 1251221319085854808,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
